8557aceb2a60c76ad1df402c47b479741ebd14f3
PugzAreCute <me@pugzarecute.com>
2024-07-07 10:48:47 +0530

    atomsync: cleanup

--- atomsync.sh
+++ atomsync.sh
@@ -5,8 +5,5 @@
 
 cd $ROM_DIRECTORY
 echo $(pwd)
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.1.txt
-sleep 20
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.2.txt
-sleep 20
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.3.txt
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all)
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) 
