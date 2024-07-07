3a235a5cdb87b55d75b9ab1cb03f597018539392
PugzAreCute <me@pugzarecute.com>
2024-07-07 10:36:02 +0530

    fixup: atomsync

--- atomsync.sh
+++ atomsync.sh
@@ -1,14 +1,8 @@
 #!/bin/bash
 # Repo sync handler. eventually this will handle failed stuffs
 
-. config.sh
-
-cd $ROM_DIRECTORY
-
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) 2>&1 | tee Sync.1.txt
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.1.txt
 sleep 20
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) 2>&1 | tee Sync.2.txt
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.2.txt
 sleep 20
-repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) 2>&1 | tee Sync.3.txt
-
-cd $SCRIPTS_DIR
+repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.3.txt
