82c8608a7c1b9b3ff30f672da78b4fd22e04bd1d
PugzAreCute <me@pugzarecute.com>
2024-07-07 17:06:16 +0530

    feat: add success/error msgs

--- build.sh
+++ build.sh
@@ -4,8 +4,18 @@
 
 cd $ROM_DIRECTORY
 . build/envsetup.sh
-MESSAGE_R=$(cat $SCRIPTS_DIR/build_start)
-MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
+
 curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
 blissify --$1 spes
+
+if [ $? == 0]; then
+MESSAGE_R=$(cat $SCRIPTS_DIR/build_success)
+MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
+curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
+else
+MESSAGE_R=$(cat $SCRIPTS_DIR/build_fail)
+MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
+echo "making sure file is not empty" >> out/error.log
+curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendDocument -F chat_id=$CHAT_ID -F caption="$MESSAGE" -F document=@out/error.log
+fi
