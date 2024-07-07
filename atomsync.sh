e1826251d4d1d3cce7c8a4bbbc3eb965d7098c2e
PugzAreCute <me@pugzarecute.com>
2024-07-07 10:37:24 +0530

    fixup atomsync: cd into rom dir

--- atomsync.sh
+++ atomsync.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 # Repo sync handler. eventually this will handle failed stuffs
 
+. config.sh
+
+cd $ROM_DIRECTORY
+
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.1.txt
 sleep 20
 repo sync -c --force-sync --optimized-fetch --no-tags --no-clone-bundle --prune -j$(nproc --all) |& tee Sync.2.txt
