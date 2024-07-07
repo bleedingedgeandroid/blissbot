ee1fb98b0550cded3b6b2c8021007a9e8a7336ad
PugzAreCute <me@pugzarecute.com>
2024-07-07 17:14:10 +0530

    build_start: use copy not cut :oops:

--- build.sh
+++ build.sh
@@ -5,6 +5,8 @@
 cd $ROM_DIRECTORY
 . build/envsetup.sh
 
+MESSAGE_R=$(cat $SCRIPTS_DIR/build_start)
+MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
 curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
 blissify --$1 spes
