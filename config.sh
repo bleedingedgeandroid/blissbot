3aba0b87d0cd6756ec1087720bf1b6e713263ffd
PugzAreCute <me@pugzarecute.com>
2024-07-09 17:34:20 +0530

    patchsource: support D1 clone

--- config.sh
+++ config.sh
@@ -1,2 +1,8 @@
 ROM_DIRECTORY="/mnt/Android/BlissRoms"
 SCRIPTS_DIR=$(pwd)
+
+
+
+addAndCommit() {
+    git add -A && git commit -m "BLISSBOT: PATCH"
+}
--- patchsource.sh
+++ patchsource.sh
@@ -5,7 +5,8 @@
 cd $ROM_DIRECTORY
 
 cd vendor/bliss
-git revert 89d3b90b677b834bf67cbc8392312f647a85d502 #qti_kernel_headers
+git apply -R https://github.com/BlissRoms/platform_vendor_bliss/commit/89d3b90b677b834bf67cbc8392312f647a85d502.patch #qti_kernel_headers
+addAndCommit
 cd ../..
 
 cd $SCRIPTS_DIR
