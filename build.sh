98faab6d30dc888519dae06ffbed22ae2cceb451
PugzAreCute <me@pugzarecute.com>
2024-07-09 17:03:37 +0530

    blissbot: set pipefail

--- build.sh
+++ build.sh
@@ -9,8 +9,9 @@ MESSAGE_R=$(cat $SCRIPTS_DIR/build_start)
 MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
 curl -s -X POST https://api.telegram.org/bot$TG_TOKEN/sendMessage -d chat_id=$CHAT_ID -d text="$MESSAGE"
 
-blissify --$1 spes |& tee build.log
 
+set -o pipefail
+blissify --$1 spes |& tee build.log
 if [ $? == 0 ]; then
 MESSAGE_R=$(cat $SCRIPTS_DIR/build_success)
 MESSAGE=${MESSAGE_R/'__VARIANT__'/"$1"}
