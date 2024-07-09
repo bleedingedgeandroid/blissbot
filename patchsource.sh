bfc044bf0ea9c17b8c008c6ca902f0d96423b5df
PugzAreCute <me@pugzarecute.com>
2024-07-09 17:54:50 +0530

    patchsource: fixup oops

--- patchsource.sh
+++ patchsource.sh
@@ -5,7 +5,7 @@
 cd $ROM_DIRECTORY
 
 cd vendor/bliss
-git apply -R https://github.com/BlissRoms/platform_vendor_bliss/commit/89d3b90b677b834bf67cbc8392312f647a85d502.patch #qti_kernel_headers
+curl -s https://github.com/BlissRoms/platform_vendor_bliss/commit/89d3b90b677b834bf67cbc8392312f647a85d502.patch | git apply -R - #qti_kernel_headers
 addAndCommit
 cd ../..
 
